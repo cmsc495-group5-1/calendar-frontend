effca59d7c177b01a9393623c23802bb82914520
Elijah Frederickson <elijah@daementor.me>
2022-06-26 10:57:46 -0400

    Add singular day name method

--- src/util/DateUtils.ts
+++ src/util/DateUtils.ts
@@ -3,9 +3,16 @@ export function getDayNames(format: "long" | "short" | "narrow" | undefined = 'l
   const formatter = new Intl.DateTimeFormat(undefined, { weekday: format, timeZone: 'UTC' });
   const days = [1, 2, 3, 4, 5, 6, 7].map(day => {
     const dd = day < 10 ? `0${day}` : day;
-    return new Date(`2017-01-${dd}T00:00:00+00:00`);
+    return new Date(`2022-01-${dd}T00:00:00+00:00`);
   });
   return days.map(date => formatter.format(date));
+}
+
+export function getDayName(day: number, format: "long" | "short" | "narrow" | undefined = 'long'): string {
+  const formatter = new Intl.DateTimeFormat(undefined, { weekday: format, timeZone: 'UTC' });
+  const dd = day < 10 ? `0${day}` : day;
+  const date = new Date(`2022-01-${dd}T00:00:00+00:00`);
+  return formatter.format(date);
 }
 
 export function isToday(day: Date): boolean {
